ece6be16f4c3d73798045e432763fd145f1ab9c5
Roberto Santalla <roobre@roobre.es>
2019-07-20 17:28:23 +0200

    Updated mockup with FA

--- src/App.js
+++ src/App.js
@@ -1,50 +1,64 @@
 import React from 'react';
-import { Container, Grid, Button, Icon, Header, Label } from 'semantic-ui-react'
+import { Container, Grid, Button, Icon, Label, Input } from 'semantic-ui-react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFan, faTintSlash } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
 function App() {
   return (
-    <Container>
+    <Container style={{ marginTop: '1em' }}>
       <Grid>
-        <Grid.Column width={16} floated='right'>
-          <Header>Daikin</Header>
+        <Grid.Column width={16} textAlign='right'>
+          <Label size='large'>AC Remote 2: Electric Boogaloo</Label>
         </Grid.Column>
 
-        <Grid.Row columns='equal'>
-          <Grid.Column stretched>
-            <Button icon={<Icon size='small' name='spinner' />} />
-          </Grid.Column>
-          <Grid.Column stretched>
-            <Button icon={<Icon size='large' name='spinner' />} />
-          </Grid.Column>
-          <Grid.Column stretched>
-            <Button icon={<Icon size='big' name='spinner' />} />
-          </Grid.Column>
-        </Grid.Row>
+        <Grid.Column width={16} stretched>
+          <Button.Group size='large'>
+            <Button><FontAwesomeIcon icon={faFan} /></Button>
+            <Button icon='snowflake outline' primary />
+            <Button><FontAwesomeIcon icon={faTintSlash} /></Button>
+          </Button.Group>
+        </Grid.Column>
 
-        <Grid.Row>
-          <Grid.Column width={11} stretched>
-            <Label size='massive'>
-              <Header size='massive' icon='temp'> 23 C</Header>
-            </Label>
-          </Grid.Column>
-          <Grid.Column width={5} stretched>
-            <Grid>
-              <Grid.Column width={16} stretched>
-                <Button icon='angle up' />
-              </Grid.Column>
-              <Grid.Column width={16} stretched>
-                <Button icon='angle down' />
-              </Grid.Column>
-            </Grid>
-          </Grid.Column>
-        </Grid.Row>
+        <Grid.Column width={16} stretched>
+          <Button.Group size='large'>
+            <Button icon={<FontAwesomeIcon icon={faFan} style={{ fontSize: '0.7em' }} />} />
+            <Button icon={<FontAwesomeIcon icon={faFan} style={{ fontSize: '0.8em' }} />} />
+            <Button icon={<FontAwesomeIcon icon={faFan} style={{ fontSize: '1em' }} />} />
+            <Button icon={<FontAwesomeIcon icon={faFan} style={{ fontSize: '1.2em' }} />} />
+            <Button icon={<FontAwesomeIcon icon={faFan} style={{ fontSize: '1.4em' }} />} />
+          </Button.Group>
+          <Button.Group size='large'>
+            <Button icon='moon' />
+            <Button content='Auto' primary />
+          </Button.Group>
+        </Grid.Column>
+
+        <Grid.Column width={16} stretched >
+          <Input size='big' type='number' labelPosition='right'>
+            <Label icon='thermometer' />
+            <input value='23' />
+            {/* <Label content='°C' /> */}
+            <Button.Group size='big'>
+              <Button icon='plus' />
+              <Button icon='minus' />
+            </Button.Group>
+          </Input>
+        </Grid.Column>
+
+        <Grid.Column width={8} stretched >
+          <Button size='large' icon='resize vertical' />
+        </Grid.Column>
+        <Grid.Column width={8} stretched >
+          <Button size='large' icon='resize horizontal' />
+        </Grid.Column>
 
         <Grid.Column width={16} stretched>
           <Button color='red' icon='power' />
         </Grid.Column>
+
       </Grid>
-    </Container>
+    </Container >
   );
 }
 
