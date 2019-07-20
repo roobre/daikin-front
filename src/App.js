ad51e1e57cdf3f93faacece617cb4ff2e4e9bbba
Roberto Santalla <roobre@roobre.es>
2019-07-20 22:24:06 +0200

    Handle asynchronous load of ac state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Container, Grid, Button, Icon, Label, Input} from 'semantic-ui-react'
+import {Container, Grid, Button, Label, Segment} from 'semantic-ui-react'
 import './controls/Mode'
 import './App.css'
 import Mode from "./controls/Mode";
@@ -9,17 +9,26 @@ import Swing from "./controls/Swing";
 
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        setTimeout(() => {
+            this.setState({
+                ac: {
+                    mode: 'cool',
+                    fanSpeed: 2,
+                    temp: 20,
+                    swing: {
+                        vertical: false,
+                        horizontal: true,
+                    },
+                    powered: true
+                }
+            })
+        }, 1000)
+    }
+
     state = {
-        ac: {
-            mode: 'cool',
-            fanSpeed: 2,
-            temp: 20,
-            swing: {
-                vertical: false,
-                horizontal: true,
-            },
-            powered: true
-        }
+        ac: null
     };
 
     pushState(acState) {
@@ -31,6 +40,23 @@ class App extends Component {
     }
 
     render() {
+        let controls = <Grid.Column width={16}><Segment placeholder loading/></Grid.Column>
+
+        if (this.state.ac != null) {
+            controls = [
+                <Mode currentMode={this.state.ac.mode} setMode={(mode) => this.pushState({mode: mode})}/>,
+                <FanSpeed currentSpeed={this.state.ac.fanSpeed}
+                          setSpeed={(speed) => this.pushState({fanSpeed: speed})}/>,
+                <Temperature currentTemp={this.state.ac.temp} setTemp={(temp) => this.pushState({temp: temp})}/>,
+                <Swing swingV={this.state.ac.swing.vertical} swingH={this.state.ac.swing.horizontal}
+                       setSwing={(v, h) => this.pushState({swing: {vertical: v, horizontal: h}})}/>,
+                <Grid.Column width={16} stretched>
+                    <Button color={this.state.ac.powered ? 'red' : 'green'} icon='power'
+                            onClick={() => this.pushState({powered: !this.state.ac.powered})}/>
+                </Grid.Column>,
+            ];
+        }
+
         return (
             <Container style={{marginTop: '1em'}}>
                 <Grid>
@@ -38,23 +64,11 @@ class App extends Component {
                         <Label size='large'>AC Remote 2: Electric Boogaloo</Label>
                     </Grid.Column>
                     <Grid.Column width={3} textAlign='right'>
-                        <Label size='large' color='green' icon={{name: 'signal', fitted: true}}/>
+                        <Label size='large' color={this.state.ac != null ? 'green' : 'red'}
+                               icon={{name: 'signal', fitted: true}}/>
                     </Grid.Column>
 
-                    <Mode currentMode={this.state.ac.mode} setMode={(mode) => this.pushState({mode: mode})}/>
-
-                    <FanSpeed currentSpeed={this.state.ac.fanSpeed}
-                              setSpeed={(speed) => this.pushState({fanSpeed: speed})}/>
-
-                    <Temperature currentTemp={this.state.ac.temp} setTemp={(temp) => this.pushState({temp: temp})}/>
-
-                    <Swing swingV={this.state.ac.swing.vertical} swingH={this.state.ac.swing.horizontal}
-                           setSwing={(v, h) => this.pushState({swing: {vertical: v, horizontal: h}})}/>
-
-                    <Grid.Column width={16} stretched>
-                        <Button color={this.state.ac.powered ? 'red' : 'green'} icon='power'
-                                onClick={() => this.pushState({powered: !this.state.ac.powered})}/>
-                    </Grid.Column>
+                    {controls}
 
                 </Grid>
             </Container>
